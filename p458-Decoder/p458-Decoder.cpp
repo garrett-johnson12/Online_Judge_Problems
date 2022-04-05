b28000bfef2fb54e1c1e68449a72de3b61a2ec59
Garrett <garrettj1337@gmail.com>
2022-04-05 13:14:24 -0500

    Upload new problem

---
+++ p458-Decoder/p458-Decoder.cpp
@@ -0,0 +1,28 @@
+// Online Judge Problem: P458 - The Decoder
+#include <iostream>
+#include <string>
+#include <fstream>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::ifstream;
+int main()
+{
+    ifstream infile;
+    infile.open("uDebug.txt");
+    int stringCount = 0;
+    string inputX;
+    while (infile >> inputX)
+    {
+        while (stringCount < inputX.length())
+        {
+            inputX[stringCount]-=7;
+            stringCount++;
+        }
+        cout << inputX << endl;
+        stringCount = 0;
+    }
+    infile.close();
+    return 0;
+}
