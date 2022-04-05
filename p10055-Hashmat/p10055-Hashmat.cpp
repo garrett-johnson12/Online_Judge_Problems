a7229f725d1a4f1032a8f44609badee82c63e213
Garrett <garrettj1337@gmail.com>
2022-04-05 09:58:07 -0500

    Updated code for accepted answer

--- p10055-Hashmat/p10055-Hashmat.cpp
+++ p10055-Hashmat/p10055-Hashmat.cpp
@@ -1,22 +1,30 @@
 // Online Judge Problem: p10055 - Hashmat the Brave Warrior
 #include <iostream>
-// #include <fstream>
 using std::cin;
 using std::cout;
 using std::endl;
-// using std::ifstream;
+
 int main()
 {
-    // ifstream infile;
-    // infile.open("uDebug.txt");
-    int has = 0;
-    int opp = 0;
+
+    long long has, opp;
 
     while (cin >> has >> opp)
     {
-        cout << abs(opp - has) << endl;
+        if (has < opp)
+        {
+            cout << opp - has << endl;
+        }
+        else
+        {
+            cout << has - opp << endl;
+        }
     }
-    cout << endl;
-    // infile.close();
+
+    // While not accepted by online judge this does generate the same answers
+    // while (cin >> has >> opp)
+    // {
+    //     cout << abs(opp - has) << endl;
+    // }
     return 0;
 }
