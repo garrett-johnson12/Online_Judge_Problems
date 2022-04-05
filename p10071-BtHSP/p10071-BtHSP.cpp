def2a6ca4efd5faa497205ec4856038ed2012c69
Garrett <garrettj1337@gmail.com>
2022-04-05 10:27:15 -0500

    Upload new problem

---
+++ p10071-BtHSP/p10071-BtHSP.cpp
@@ -0,0 +1,20 @@
+// Online Judge Problem: p10071 - Back to High School Physics
+#include <iostream>
+// #include <fstream>
+using std::cin;
+using std::cout;
+using std::endl;
+// using std::ifstream;
+
+int main()
+{
+    // ifstream infile;
+    // infile.open("uDebug-Input.txt");
+    int x, y;
+    while (cin >> x >> y)
+    {
+        cout << (2 * x) * y << endl;
+    }
+    // infile.close();
+    return 0;
+}
